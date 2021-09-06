cac71719b046ea934536f82d38c1b2d3577bdf1f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-06 18:45:02 +0300

    Initial Commit

---
+++ CustomAnyTransition/CustomAnyTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnyTransitionApp.swift
+//  CustomAnyTransition
+//
+//  Created by ramil on 06.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnyTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
