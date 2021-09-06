32ddd6c2c31db8f7d86e2654afdebecadd9021b8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-06 19:13:07 +0300

    init

--- CustomAnyTransition/ContentView.swift
+++ CustomAnyTransition/ContentView.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+struct RotateViewModifier: ViewModifier {
+    
+    let rotation: Double
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(Angle(degrees: rotation))
+            .offset(
+                x: rotation != 0 ? UIScreen.main.bounds.width : 0,
+                y: rotation != 0 ? UIScreen.main.bounds.height : 0
+            )
+    }
+}
+
+extension AnyTransition {
+    
+    static var rotating: AnyTransition {
+        return modifier(active: RotateViewModifier(rotation: 180), identity: RotateViewModifier(rotation: 0))
+    }
+    
+    static func rotating(rotation: Double) -> AnyTransition {
+        return modifier(active: RotateViewModifier(rotation: rotation), identity: RotateViewModifier(rotation: 0))
+    }
+    
+    static var rotateOn: AnyTransition {
+        return asymmetric(
+            insertion: .rotating,
+            removal: .move(edge: .leading)
+        )
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var showRectangle: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if showRectangle {
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 250, height: 350)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .transition(.rotateOn)
+            }
+            
+            Spacer()
+            
+            Text("Click Me!")
+                .padding()
+                .onTapGesture {
+                    withAnimation(.easeInOut) {
+                        showRectangle.toggle()
+                    }
+                }
+        }
     }
 }
 
